d1e7efa816df604bb1a3de01518dc45e21599347
Viniq12k <vinizn18@gmail.com>
2026-05-11 13:13:47 -0300

    Atualizar o script.js

--- script.js
+++ script.js
@@ -93,6 +93,19 @@ function criarCartas() {
       }
     });
   }
+
+  // Revela todas as cartas no início
+  lockBoard = true;
+  const todasCartas = document.querySelectorAll(".carta");
+  for (let i = 0; i < todasCartas.length; i++) {
+    todasCartas[i].textContent = todasCartas[i].dataset.value;
+  }
+  setTimeout(() => {
+    for (let i = 0; i < todasCartas.length; i++) {
+      todasCartas[i].textContent = "?";
+    }
+    lockBoard = false;
+  }, 2000);
 }
 // Função para avançar para o próximo nível, resetando o tabuleiro e atualizando as informações do jogo;
 function proximoNivel() {
